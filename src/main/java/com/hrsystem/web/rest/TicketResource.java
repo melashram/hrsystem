a10f2332a7a686b8770e224c63e533c99eeb3123
Mariam.ElAshram <m.elashram@qcentris.com>
2018-03-06 11:09:53 +0200

    remove creation date from dialog box add creation date instant.now()

--- src/main/java/com/hrsystem/web/rest/TicketResource.java
+++ src/main/java/com/hrsystem/web/rest/TicketResource.java
@@ -16,6 +16,7 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -50,6 +51,7 @@ public class TicketResource {
         if (ticket.getId() != null) {
             throw new BadRequestAlertException("A new ticket cannot already have an ID", ENTITY_NAME, "idexists");
         }
+        ticket.setCreationdate(Instant.now());
         Ticket result = ticketRepository.save(ticket);
         return ResponseEntity.created(new URI("/api/tickets/" + result.getId()))
             .headers(HeaderUtil.createEntityCreationAlert(ENTITY_NAME, result.getId().toString()))
