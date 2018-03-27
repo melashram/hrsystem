27e4aee47da481d38a09cfa386ead06550de5fe7
Mariam.ElAshram <m.elashram@qcentris.com>
2018-03-27 13:45:01 +0200

    27-3-2018 before adding authorties to request

---
+++ src/main/webapp/app/create-ticket/create-ticket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-create-ticket',
+  templateUrl: './create-ticket.component.html',
+  styles: []
+})
+export class CreateTicketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
